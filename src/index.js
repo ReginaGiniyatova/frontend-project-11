1a06aa98ec4002136e427f2da589dec55c9ad505
Regina Giniyatova <giniyatovar@yandex.ru>
2025-04-15 21:43:56 +0300

    steps 4&&5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import onChange from 'on-change';
-import { handleValidateFailed, handleValidateSuccess, runStartRender } from './watchers.js';
+import axios from 'axios';
+import { validateRssInput, runStartRender, renderRssPosts } from './watchers.js';
 import { object, string } from 'yup';
 import i18next from 'i18next';
+import { nanoid } from 'nanoid'
 import resources from './localization/languages.js';
 
 const urlSchema = object({
@@ -21,6 +23,7 @@ function runApp() {
 
   const state = {
     posts: [],
+    feeds: [],
     uiState: {
       validate: {
         isValid: null,
@@ -30,31 +33,127 @@ function runApp() {
   }
 
   const watchedValidate = onChange(state.uiState.validate, (path, value) => {
-    console.log('path: ', path)
-    console.log('value: ', value)
-    handleValidateFailed(path, value, i18nInstance)
+    validateRssInput(path, value, i18nInstance)
   })
+
+  const watchedState = onChange(state, (path, value) => {
+    renderRssPosts(path, value, i18nInstance)
+  })
+
+  const setValidateFields = (isValid, message) => {
+    watchedValidate.isValid = false;
+
+    watchedValidate.isValid = isValid
+    watchedValidate.message = message
+  }
+
+  const refreshRssFeed = (list) => {
+    setTimeout(() => {
+      const promises = list.map(url => axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${url}`));
+      console.log(list);
+
+      Promise.all(promises)
+        .then(data => {
+          data.forEach(response => {
+            const result = parseRssResponse(response);
+            const newPosts = result.posts.filter(post => !watchedState.posts.some(p => post.link === p.link));
+
+            if(newPosts.length > 0) watchedState.posts = [ ...newPosts, ...watchedState.posts ];
+          });
+
+          refreshRssFeed(list);
+        })
+        .catch(error => console.log(error));
+    }, 5000);
+  }
 
   const arr = []
   const validate = (url) => {
     return new Promise((resolve, error) => {
         if(arr.includes(url)) {
-            // error('Адрес уже был добавлен!')
-            watchedValidate.message = 'rssAlreadyAdded'
-            return
+          setValidateFields(false, 'rssAlreadyAdded')
+          return
         }
         urlSchema.validate({url})
-        .then(_ => {
-            arr.push(url)
-            resolve()
-        })
-        // .catch(_ => error('Неверный формат RSS ленты'))
-        .catch(_ => {
-          watchedValidate.isValid = false
-          watchedValidate.message = 'invalidUrl'
-          // error(state.uiState.validate.message)
-        })
+          .then(_ => {
+            isUrlHasRssFeed(url)
+              .then(_ => {
+                arr.push(url)
+                setValidateFields(true, 'success');
+                resolve(url)
+              })
+              .catch(errorMesssage => setValidateFields(false, errorMesssage));
+          })
+          .catch(_ => setValidateFields(false,'invalidUrl'))
     })
+  }
+
+  function isUrlHasRssFeed (url) {
+    return new Promise((resolve, error) => {
+      axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${url}`)
+        .then(response => {
+          if(response.status != 200 && response.status != 201) {
+            error('connectionError');
+            return
+          }
+
+          const parser = new DOMParser()
+          const data = parser.parseFromString(response.data.contents, 'text/xml')
+
+          try {
+            const feedTitle = data.querySelector('channel > title').textContent
+            resolve(url)
+          } catch(err) {
+            error('noValidRss')
+          }
+        })
+        .catch(e => error('connectionError'))
+    })
+  }
+
+  const parseRssResponse = (response) => {
+      const nanoId = nanoid(6)
+      const parser = new DOMParser()
+      const data = parser.parseFromString(response.data.contents, 'text/xml')
+
+      const feedTitle = data.querySelector('channel > title')?.textContent ?? ''
+      const feedDescription = data.querySelector('channel > description')?.textContent ?? ''
+      const postsXML = data.querySelectorAll('channel > item')
+
+      const feed = {
+        id: nanoId,
+        title: feedTitle,
+        description: feedDescription
+      };
+
+      const posts = []
+
+      postsXML.forEach(item => {
+        posts.push({
+          id: nanoId,
+          title: item.querySelector('title').textContent,
+          description: item.querySelector('description').textContent,
+          link: item.querySelector('link').textContent
+        })
+      });
+
+      return ({
+        nanoId,
+        feed,
+        posts,
+      });
+  }
+
+
+  function getDataPosts(url) {
+    axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${url}`)
+      .then(function (response) {
+        const result = parseRssResponse(response);
+
+        watchedState.feeds = [ result.feed, ...watchedState.feeds ];         
+        watchedState.posts = [ ...result.posts, ...watchedState.posts ];
+      })
+      .catch(_ => setValidateFields(false, 'connectionError'));
   }
 
   form.addEventListener('submit', (e) => {
@@ -62,9 +161,11 @@ function runApp() {
     const data = new FormData(form);
 
     validate(data.get('url')) 
-    .then(_ => handleValidateSuccess(i18nInstance))
-    // .catch(message => handleValidateFailed(message))
+      .then(url => getDataPosts(url))
+    //.catch(message => handleValidateFailed(message))
   })
+
+  refreshRssFeed(arr);
 }
 
 runApp()
--- src/watchers.js
+++ src/watchers.js
@@ -2,47 +2,109 @@ const form = document.querySelector('#form');
 const errorMessage = document.querySelector('#error_message')
 
 const runStartRender = (i18n) => {
-const title = document.querySelector('#title')
-const desc = document.querySelector('#description')
-const label = document.querySelector('#label')
-const btn = document.querySelector('#btnAdded')
+  const title = document.querySelector('#title')
+  const desc = document.querySelector('#description')
+  const label = document.querySelector('#label')
+  const btn = document.querySelector('#addButton')
 
-title.textContent = i18n.t('header')
+  title.textContent = i18n.t('header')
 
-desc.textContent = i18n.t('description');
-label.textContent = i18n.t('label');
-btn.textContent = i18n.t('button');
+  desc.textContent = i18n.t('description');
+  label.textContent = i18n.t('label');
+  btn.textContent = i18n.t('button');
 }
 
-const handleValidateFailed = (path, value, i18n) => {
-    if (path === 'isValid') {
-      const input = document.querySelector('#url-input')
-      input.classList.add('border');
-      input.classList.add('border-danger');
-      input.classList.add('border-2');
-      errorMessage.classList.add('invisible')
-      errorMessage.classList.remove('visible')
-    } else if (path === 'message') {
-      console.log(i18n.t(`feedback.${value}`))
-      errorMessage.classList.remove('invisible')
-      errorMessage.textContent = i18n.t(`feedback.${value}`)
-    }
+const handleValidateState = (isValid, i18n) => {
+  const input = document.querySelector('#url-input')
+
+  if(!isValid) {
+    input.classList.add('border', 'border-danger', 'border-2');
+    errorMessage.classList.add('invisible', 'text-danger')
+    errorMessage.classList.remove('visible', 'invisible')
+    return
+  }
+  
+  input.classList.remove('border', 'border-danger', 'border-2');
+  form.reset()
+  input.focus()
+
+  errorMessage.classList.remove('text-danger', 'invisible')
+  errorMessage.classList.add('text-success', 'visible')
 }
 
-const handleValidateSuccess = (i18n) => {
-    const input = document.querySelector('#url-input')
-    input.classList.remove('border');
-    input.classList.remove('border-danger');
-    input.classList.remove('border-2');
-    form.reset()
-    input.focus()
-
-    errorMessage.classList.remove('text-danger')
-    errorMessage.classList.add('text-success')
-    errorMessage.classList.remove('invisible')
-    errorMessage.classList.add('visible')
-    errorMessage.textContent = i18n.t('feedback.success')
+const handleValidateMessage = (message, i18n) => {
+  errorMessage.textContent = i18n.t(`feedback.${message}`)
 }
 
+const rssValidationActions = {
+  'isValid': handleValidateState,
+  'message': handleValidateMessage,
+}
 
-export { handleValidateFailed, handleValidateSuccess, runStartRender }
+const validateRssInput = (path, value, i18n) => {
+  return rssValidationActions[path](value, i18n);
+}
+
+const renderFeeds = (container, titleElement, items, i18n) => {
+  container.innerHTML = '';
+
+  titleElement.textContent = i18n.t('rss.feeds');
+
+  items.forEach(feed => {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'border-0', 'border-end-0');
+
+    const h3 = document.createElement('h3');
+    h3.classList.add('h6', 'm-0');
+    h3.textContent = feed.title;
+
+    const p = document.createElement('p');
+    p.classList.add('m-0', 'small', 'text-black-50');
+    p.textContent = feed.description;
+
+    li.append(h3, p);
+
+    container.append(li);
+  })
+}
+
+const renderPosts = (container, titleElement, items, i18n) => {
+  container.innerHTML = '';
+
+  titleElement.textContent = i18n.t('rss.posts');
+  items.forEach(post => {
+    const div = document.createElement('div')
+    div.classList.add('d-flex', 'justify-content-between', 'align-items-start', 'mb-4')
+
+    const cardLink = document.createElement('a')
+    const cardBtn = document.createElement('button')
+
+    cardBtn.classList.add('btn', 'btn-outline-primary', 'btn-sm')
+
+    cardLink.textContent = post.title
+    cardLink.href = post.link
+    cardLink.setAttribute('target', '_blank');
+    cardLink.classList.add('fw-bold');
+
+    cardBtn.textContent = i18n.t('rss.linkBtn')
+
+    div.append(cardLink, cardBtn)
+
+    container.append(div)
+  })
+}
+
+const renderRssPosts = (path, value, i18n) => {
+  const postsCardTitle = document.querySelector('.card-title');
+  const feedsCardTitle = document.querySelector('.feed-title');
+  const posts = document.querySelector('#posts')
+  const feeds = document.querySelector('#feeds')
+
+  switch(path) {
+    case 'feeds': renderFeeds(feeds, feedsCardTitle, value, i18n); break;
+    case 'posts': renderPosts(posts, postsCardTitle, value, i18n); break;
+    default: console.error(`Invalid path: ${path}, value: ${value}`); break;
+  }
+}
+
+export { validateRssInput, runStartRender, renderRssPosts }
